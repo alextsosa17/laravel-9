e6d83cded56bfecb32212fc8d5ea6fb3e420bba8
alextsosa17 <asosa@estudiantes.unsam.edu.ar>
2023-06-24 22:18:47 -0300

    se agrego un componente en la vista welcome , el componente esta en la carpeta components

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <meta name="meta-description" content= "@yield('meta-desciption', 'Default meta description')">
+</head>
+
+<body>
+
+    <!-- {{--@include('partials.navigation') {{--menu de navegacion--}}--}} -->
+    {{-- aca estoy utilizando un  comentario con el modelo de plantillas blade--}}
+    {{$slot}} <!-- asi imprimo la variable reservada para mis componentes s-->
+
+     <!-- //aqui queremos que se imprima el contenido variable de la plantilla -->
+
+
+
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,12 @@
-    @extends('layouts.app')
+    <!-- @extends('layouts.app')
     @section('title','Home')
-    @section('meta-desciption', 'Home meta-description')
+    @section('meta-desciption', 'Home meta-description') -->
         
-    @section('content')
+    <!-- @section('content') -->
+    <x-layout><!-- esta sintaxis ya asume que el componente esta en la carpeta components en resources/views -->
+    @component('components.layout') <!-- con esta sintaxis tambien se puede usar componentes-->
     <h1>Inicio</h1>
-    @endsection
+    <!-- @endcomponent   recordar cerrar el componente-->
+    </x-layout> <!-- solo hay que agregarle el archivo donde se va a imprimir ese componente-->
+    <!-- @endsection -->
+  
