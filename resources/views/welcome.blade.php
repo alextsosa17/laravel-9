ecf37ff22389d6743e250a78dcf16767333e61a6
alextsosa17 <asosa@estudiantes.unsam.edu.ar>
2023-06-24 23:35:46 -0300

    video 9 minuto 4:09

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="Home" > {{--puedo solamente agregarle atributos y usarlos como variables en mi vista principal --}}
+<x-layout title="Home" > {{--puedo solamente agregarle atributos y usarlos como variables en mi vista principal }}
 
     {{--<x-slot name="title">
         home    
